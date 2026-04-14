55f042bd44d28bfbfee9caaa923a7224005bc2b9
tmak1 <tarik.khan.git@gmail.com>
2026-04-14 17:33:23 +1000

    correct syntax

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
-#!/usr/bin env bash
+#!/usr/bin/env bash
 
 export AWS_PROFILE=root-adm
 echo "deploying with profile $AWS_PROFILE"
 
 aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 978344289623.dkr.ecr.ap-southeast-2.amazonaws.com
 
-docker build -t 978344289623.dkr.ecr.ap-southeast-2.amazonaws.com/mongo-back:latest
+docker build -t 978344289623.dkr.ecr.ap-southeast-2.amazonaws.com/mongo-back .
 
 docker push 978344289623.dkr.ecr.ap-southeast-2.amazonaws.com/mongo-back:latest
+
+docker rmi 978344289623.dkr.ecr.ap-southeast-2.amazonaws.com/mongo-back:latest
