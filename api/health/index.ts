08552c5edc48d5d2878b9263e0fb2804294e5ece
tmak1 <tarik.khan.git@gmail.com>
2026-04-29 06:13:34 +1000

    test commit

--- api/health/index.ts
+++ api/health/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", async (_req: express.Request, res: express.Response) => {
-  res.status(200).send({ health: "ok" });
+  res.status(200).send({ health: "healthy" });
 });
 
 export default router;
